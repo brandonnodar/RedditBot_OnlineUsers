93b77a0d3b2f0105505a3dd33cc24e83b2a9394e
TinyQubit <brandonnodar@gmail.com>
2019-03-23 18:17:59 -0400

    Add files via upload

--- Scripts/Subreddit_Online_Users_Grapher.py
+++ Scripts/Subreddit_Online_Users_Grapher.py
@@ -9,7 +9,7 @@ subreddit_name = "gamedev"
 date_to_collect = "03-22-2019" # Make sure format is: MM-DD-YYYY
 
 # BE CAUTIOUS EDITING
-root_directory = "~/Desktop/Reddit_Bot/"
+root_directory = "~/Desktop/RedditBot_OnlineUsers/"
 
 # Initializations. DO NOT EDIT UNLESS LAST RESORT.
 data_x = []
@@ -27,12 +27,6 @@ def GrabDataFromTextFile():
     with open(file_path, "r") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter = ',')
         for row in csv_reader:
-            '''
-            hours, minutes, seconds = map(int, row[0].split(":"))
-            str_hours = str(hours) + "." + str(minutes)
-            float_hours = float(str_hours)
-            data_x.append(float_hours)
-            '''
             data_x.append(row[0])
             online_count = int(row[1])
             data_y.append(online_count)
@@ -43,31 +37,6 @@ def GraphData():
     global subreddit_name
 
     print("Graphing data now...")
-
-    '''
-    x = np.array(data_x)
-    test_y = [500, 1000, 1500, 2000, 2500, 3000]
-    y = np.array(test_y)
-    
-    plt.xlabel('Time')
-    plt.ylabel('Up Votes')
-    plt.title("Subreddit: " + str(subreddit_name))
-    '''
-    
-    '''
-    plt.plot(data_x, data_y, c="#ff4400")
-    
-    plt.title("Subreddit: " + str(subreddit_name))
-    plt.xlabel("Time")
-    plt.ylabel("Up Votes")
-    
-    plt.xticks(rotation=90)
-    
-    
-    plt.locator_params(numticks = 10)
-    plt.show()
-    '''
-    
     
     x = data_x
     y = data_y
@@ -84,13 +53,8 @@ def GraphData():
             if i % every_nth != 0:
                 line.set_visible(False)
         
-
     plt.xticks(fontsize=8, rotation=60)
     plt.show()
-
-
-
-    
 
 GrabDataFromTextFile()
 GraphData()        
